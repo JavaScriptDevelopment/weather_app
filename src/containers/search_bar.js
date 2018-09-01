079db32ff3ada06de7300767aeaba616a238bd3a
A.Rupesh <a.rupesh66@gmail.com>
2018-09-02 03:37:14 +0530

    Update search_bar.js

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,1 +1,17 @@
 
+import React from 'react';
+import { Component } from 'react';
+
+inport SearchBar from '../containers/search_bar.js';
+render(){
+
+    return (
+      <div>
+      <SearchBar />  
+      </div>
+
+    );
+  
+  }
+
+}
