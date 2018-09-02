5d602e606166163a494679ccf6a349d41d1ecde4
A.Rupesh <a.rupesh66@gmail.com>
2018-09-02 07:33:22 +0530

    final touchups

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,15 +2,18 @@ import axios from 'axios';
 
 const API_KEY = '00ff99ebcfce2e0569acd627d923fb8d';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-    const url = `${ROOT_URL}&q=${city},india`;
-    const request = axios.get(url);
-    
-    return {
-        type: FETCH_WEATHER,
-        payload: request
-    };
-}
+  //refer to documentation to see format for api request
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
 
+  console.log('Request: ', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 
-
-
 import App from './components/app';
 import reducers from './reducers';
 
