6257eff5180eb00984c1e1b7b88f77d5dddeb8bf
A.Rupesh <a.rupesh66@gmail.com>
2018-09-02 06:26:08 +0530

    Update reducer_weather.js

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,9 @@
+import { FETCH_WEATHER  } from '../actions/index';
 
 export default function(state = null, action) {
-   
+    switch (action.type) {
+    case FETCH_WEATHER:
+        return state.push(action.payload.data);
+}
     return state;
 }
